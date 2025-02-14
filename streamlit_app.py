c9b359c628be1a167bcdf4d32393eddd4ea397b0
Ivan-Firefly <90849921+Ivan-Firefly@users.noreply.github.com>
2025-02-14 13:47:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,22 @@
 import streamlit as st
-
-st.title("🎈 My new pp")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-
-import streamlit as st
 import pandas as pd
+from main import main
 
-uploaded_file = st.file_uploader("Choose a file")
+st.title("TCPro to DXF converter")
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
+
+uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
 if uploaded_file is not None:
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    dxf_data = main(uploaded_file)  # Get binary DXF data
+
+    if dxf_data:
+        st.download_button(
+            label="Download DXF",
+            data=dxf_data,
+            file_name="output.dxf",
+            mime="application/dxf"
+        )
+    else:
+        st.error("Error: Failed to generate DXF file.")
